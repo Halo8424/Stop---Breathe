4e5a343e89dc4b2150191e92e4493d729241fb03
Richard.C <r.conde@live.com>
2020-05-26 16:22:47 -0400

    delete html text, global variables

--- logic.js
+++ logic.js
@@ -1,3 +1,11 @@
 //  Dom Elements
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+
+// Variables
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+// Check
+console.log(breatheTime, holdTime);
